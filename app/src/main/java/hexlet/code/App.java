545e262e98c98688caaf57b497cbed126935094d
zHd4 <computersystembase@gmail.com>
2023-10-10 22:57:57 +0200

    Implemented help message displaying (with picocli) in hexlet.code.App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,24 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import java.util.concurrent.Callable;
+
+@CommandLine.Command(name="gendiff", description="Compares two configuration files and shows a difference.",
+        mixinStandardHelpOptions=true)
+public class App implements Callable<Integer> {
+    @CommandLine.Option(names={ "-h", "--help" }, usageHelp=true, description="Show this help message and exit.")
+    private boolean helpRequested = false;
+
+    @CommandLine.Option(names={ "-V", "--version" }, versionHelp=true, description="Print version information and exit.")
+    private boolean versionRequested = false;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        return 0;
     }
 }
