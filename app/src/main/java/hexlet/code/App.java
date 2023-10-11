52571c98da3ec24dee8c5f70af82e2f010e36498
zHd4 <computersystembase@gmail.com>
2023-10-11 20:16:17 +0200

    Added (-f, --format=<format>) flag to help message in hexlet.code.App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,14 +3,25 @@ package hexlet.code;
 import picocli.CommandLine;
 import java.util.concurrent.Callable;
 
-@CommandLine.Command(name="gendiff", description="Compares two configuration files and shows a difference.",
+@CommandLine.Command(name= "gendiff" , description = "Compares two configuration files and shows a difference.",
         mixinStandardHelpOptions=true)
 public class App implements Callable<Integer> {
-    @CommandLine.Option(names={ "-h", "--help" }, usageHelp=true, description="Show this help message and exit.")
-    private boolean helpRequested = false;
+    @CommandLine.Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+    private String filePath1;
 
-    @CommandLine.Option(names={ "-V", "--version" }, versionHelp=true, description="Print version information and exit.")
-    private boolean versionRequested = false;
+    @CommandLine.Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
+    private String filePath2;
+
+    @CommandLine.Option(names = { "-f", "--format" }, description = "output format [default: stylish]")
+    private String format = "stylish";
+
+    @CommandLine.Option(names = { "-h", "--help" }, usageHelp = true,
+            description = "Show this help message and exit.")
+    private boolean helpRequested;
+
+    @CommandLine.Option(names = { "-V", "--version" }, versionHelp = true,
+            description = "Print version information and exit.")
+    private boolean versionRequested;
 
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
