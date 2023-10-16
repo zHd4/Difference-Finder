15da7340ff906ece93afdc2de52557a793afdfed
zHd4 <computersystembase@gmail.com>
2023-10-16 22:22:53 +0200

    Refactor code style in DifferTest

--- app/src/test/java/DifferTest.java
+++ app/src/test/java/DifferTest.java
@@ -1,9 +1,8 @@
 import static org.assertj.core.api.Assertions.assertThat;
 
 import hexlet.code.Differ;
-import org.junit.jupiter.api.Test;
-
 import java.io.IOException;
+import org.junit.jupiter.api.Test;
 
 public class DifferTest {
     @Test
