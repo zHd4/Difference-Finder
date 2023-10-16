3990098acbbe3d56651ab4f68ffc7b4020abf756
zHd4 <computersystembase@gmail.com>
2023-10-16 22:17:56 +0200

    Add DifferTest

---
+++ app/src/test/java/DifferTest.java
@@ -0,0 +1,27 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
+import hexlet.code.Differ;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class DifferTest {
+    @Test
+    void testGenerate() throws IOException {
+        String filePath1 = "./src/test/resources/file1.json";
+        String filePath2 = "./src/test/resources/file2.json";
+
+        String actual = Differ.generate(filePath1, filePath2);
+        String expected = """
+                {
+                  - follow: false
+                    host: hexlet.io
+                  - proxy: 123.234.53.22
+                  - timeout: 50
+                  + timeout: 20
+                  + verbose: true
+                }""";
+
+        assertThat(actual).isEqualTo(expected);
+    }
+}
