a674816f42a6b63aba132489e667573bae28fef4
zHd4 <computersystembase@gmail.com>
2023-10-19 23:13:11 +0200

    Implement YamlDiffer

--- app/src/main/java/hexlet/code/differs/YamlDiffer.java
+++ app/src/main/java/hexlet/code/differs/YamlDiffer.java
@@ -1,10 +1,16 @@
 package hexlet.code.differs;
 
+import hexlet.code.Parser;
+
 import java.io.IOException;
+import java.util.Map;
 
 public class YamlDiffer implements IDiffer{
     @Override
     public String generate(String path1, String path2) throws IOException {
-        return null;
+        Map<String, Object> map1 = Parser.parseFlatYamlFile(path1);
+        Map<String, Object> map2 = Parser.parseFlatYamlFile(path2);
+
+        return FlatMapDiffer.diff(map1, map2);
     }
 }
