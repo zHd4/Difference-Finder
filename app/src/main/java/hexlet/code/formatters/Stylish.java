040ff4e345bfa4995a285be0194032526deb17ae
zHd4 <computersystembase@gmail.com>
2023-10-20 21:56:58 +0200

    Add Stylish

---
+++ app/src/main/java/hexlet/code/formatters/Stylish.java
@@ -0,0 +1,12 @@
+package hexlet.code.formatters;
+
+import java.util.List;
+import java.util.Map;
+
+public class Stylish implements IFormatter{
+
+    @Override
+    public String formatDiff(List<Map<Object, Object>> diff) {
+        return null;
+    }
+}
