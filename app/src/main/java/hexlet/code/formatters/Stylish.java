2ff4ce908947ea9fcc0dea339b922cc2e9210d91
zHd4 <computersystembase@gmail.com>
2023-10-22 01:05:24 +0200

    Implement Stylish

--- app/src/main/java/hexlet/code/formatters/Stylish.java
+++ app/src/main/java/hexlet/code/formatters/Stylish.java
@@ -7,6 +7,51 @@ public class Stylish implements IFormatter{
 
     @Override
     public String formatDiff(List<Map<Object, Object>> diff) {
-        return null;
+        StringBuilder result = new StringBuilder("{\n");
+
+        for (Map<Object, Object> changeMap : diff) {
+            String type = String.valueOf(changeMap.get("diff"));
+            String key = String.valueOf(changeMap.get("key"));
+
+            String value = valueToString(changeMap.get(changeMap.containsKey("value") ? "value" : "value1"));
+            String value2 = changeMap.containsKey("value2") ? valueToString(changeMap.get("value2")) : null;
+
+            switch (type) {
+                case "added" -> result.append("  + ")
+                        .append(key)
+                        .append(": ")
+                        .append(value)
+                        .append("\n");
+                case "deleted" -> result.append("  - ")
+                        .append(key)
+                        .append(": ")
+                        .append(value)
+                        .append("\n");
+                case "changed" -> {
+                    result.append("  - ")
+                            .append(key)
+                            .append(": ")
+                            .append(value)
+                            .append("\n");
+                    result.append("  + ")
+                            .append(key)
+                            .append(": ")
+                            .append(value2)
+                            .append("\n");
+                }
+                case "unchanged" -> result.append("    ")
+                        .append(key)
+                        .append(": ")
+                        .append(value)
+                        .append("\n");
+            }
+        }
+
+        result.append("}");
+        return result.toString();
+    }
+
+    private String valueToString(Object value) {
+        return value != null ? value.toString() : "null";
     }
 }
