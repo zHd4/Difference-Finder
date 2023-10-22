358370afc7f1f7416765f4f0dae2121cd0928aaa
zHd4 <computersystembase@gmail.com>
2023-10-22 22:32:08 +0200

    Implement tests for plain format

--- app/src/test/java/DifferTest.java
+++ app/src/test/java/DifferTest.java
@@ -2,10 +2,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import hexlet.code.Differ;
 import java.io.IOException;
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
 
 public class DifferTest {
-    private static final String expectedDiff = """
+    private static final Map<String, String> jsonFilesPaths = Map.of(
+            "file1", "./src/test/resources/file1.json",
+            "file2", "./src/test/resources/file2.json"
+    );
+
+    private static final Map<String, String> yamlFilesPaths = Map.of(
+            "file1", "./src/test/resources/file1.yml",
+            "file2", "./src/test/resources/file2.yml"
+    );
+
+    private static final String expectedStylishDiff = """
             {
                 chars1: [a, b, c]
               - chars2: [d, e, f]
@@ -32,23 +44,50 @@ public class DifferTest {
               + setting3: none
             }""";
 
+    private static final String expectedPlainDiff = """
+            Property 'chars2' was updated. From [complex value] to false
+            Property 'checked' was updated. From false to true
+            Property 'default' was updated. From null to [complex value]
+            Property 'id' was updated. From 45 to null
+            Property 'key1' was removed
+            Property 'key2' was added with value: 'value2'
+            Property 'numbers2' was updated. From [complex value] to [complex value]
+            Property 'numbers3' was removed
+            Property 'numbers4' was added with value: [complex value]
+            Property 'obj1' was added with value: [complex value]
+            Property 'setting1' was updated. From 'Some value' to 'Another value'
+            Property 'setting2' was updated. From 200 to 300
+            Property 'setting3' was updated. From true to 'none'""";
+
     @Test
-    void testJsonDiff() throws IOException {
-        String filePath1 = "./src/test/resources/file1.json";
-        String filePath2 = "./src/test/resources/file2.json";
-
-        String actual = Differ.generate(filePath1, filePath2, "stylish");
-
-        assertEquals(expectedDiff, actual);
+    void testJsonStylishDiff() throws IOException {
+        String actual = Differ.generate(jsonFilesPaths.get("file1"),
+                jsonFilesPaths.get("file2"),
+                "stylish");
+        assertEquals(expectedStylishDiff, actual);
     }
 
     @Test
-    void testYamlDiff() throws IOException {
-        String filePath1 = "./src/test/resources/file1.yml";
-        String filePath2 = "./src/test/resources/file2.yml";
+    void testYamlStylishDiff() throws IOException {
+        String actual = Differ.generate(yamlFilesPaths.get("file1"),
+                yamlFilesPaths.get("file2"),
+                "stylish");
+        assertEquals(expectedStylishDiff, actual);
+    }
 
-        String actual = Differ.generate(filePath1, filePath2, "stylish");
+    @Test
+    void testJsonPlainDiff() throws IOException {
+        String actual = Differ.generate(jsonFilesPaths.get("file1"),
+                jsonFilesPaths.get("file2"),
+                "plain");
+        assertEquals(expectedPlainDiff, actual);
+    }
 
-        assertEquals(expectedDiff, actual);
+    @Test
+    void testYamlPlainDiff() throws IOException {
+        String actual = Differ.generate(yamlFilesPaths.get("file1"),
+                yamlFilesPaths.get("file2"),
+                "plain");
+        assertEquals(expectedPlainDiff, actual);
     }
 }
