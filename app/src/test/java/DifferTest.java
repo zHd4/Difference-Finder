60f0d18f836dfe4d6db168829dc807eed894edb8
zHd4 <computersystembase@gmail.com>
2023-10-28 03:37:43 +0200

    Refactor DifferTest

--- app/src/test/java/DifferTest.java
+++ app/src/test/java/DifferTest.java
@@ -4,22 +4,11 @@ import hexlet.code.Differ;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Map;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class DifferTest {
-    private static final Map<String, String> JSON_FILES_PATHS = Map.of(
-            "file1", "./src/test/resources/fixtures/file1.json",
-            "file2", "./src/test/resources/fixtures/file2.json"
-    );
-
-    private static final Map<String, String> YAML_FILES_PATHS = Map.of(
-            "file1", "./src/test/resources/fixtures/file1.yml",
-            "file2", "./src/test/resources/fixtures/file2.yml"
-    );
-
     private static String stylishExpected;
     private static String plainExpected;
     private static String jsonExpected;
@@ -32,51 +21,55 @@ public class DifferTest {
     }
 
     private static String readFixture(String filename) throws IOException {
-        return Files.readString(Path.of("src/test/resources/fixtures/" + filename));
+        return Files.readString(Path.of(generateFixturePath(filename)));
+    }
+
+    private static String generateFixturePath(String filename) {
+        return Path.of("src/test/resources/fixtures/" + filename).toString();
     }
 
     @Test
     void testJsonStylishDiff() throws IOException {
-        String actual = Differ.generate(JSON_FILES_PATHS.get("file1"),
-                JSON_FILES_PATHS.get("file2"));
+        String actual = Differ.generate(generateFixturePath("file1.json"),
+                generateFixturePath("file2.json"));
         assertEquals(stylishExpected, actual);
     }
 
     @Test
     void testYamlStylishDiff() throws IOException {
-        String actual = Differ.generate(YAML_FILES_PATHS.get("file1"),
-                YAML_FILES_PATHS.get("file2"));
+        String actual = Differ.generate(generateFixturePath("file1.yml"),
+                generateFixturePath("file2.yml"));
         assertEquals(stylishExpected, actual);
     }
 
     @Test
     void testJsonPlainDiff() throws IOException {
-        String actual = Differ.generate(JSON_FILES_PATHS.get("file1"),
-                JSON_FILES_PATHS.get("file2"),
+        String actual = Differ.generate(generateFixturePath("file1.json"),
+                generateFixturePath("file2.json"),
                 "plain");
         assertEquals(plainExpected, actual);
     }
 
     @Test
     void testYamlPlainDiff() throws IOException {
-        String actual = Differ.generate(YAML_FILES_PATHS.get("file1"),
-                YAML_FILES_PATHS.get("file2"),
+        String actual = Differ.generate(generateFixturePath("file1.yml"),
+                generateFixturePath("file2.yml"),
                 "plain");
         assertEquals(plainExpected, actual);
     }
 
     @Test
     void testJsonFormatDiff() throws IOException {
-        String actual = Differ.generate(JSON_FILES_PATHS.get("file1"),
-                JSON_FILES_PATHS.get("file2"),
+        String actual = Differ.generate(generateFixturePath("file1.json"),
+                generateFixturePath("file2.json"),
                 "json");
         assertEquals(jsonExpected, actual);
     }
 
     @Test
     void testYamlJsonFormatDiff() throws IOException {
-        String actual = Differ.generate(YAML_FILES_PATHS.get("file1"),
-                YAML_FILES_PATHS.get("file2"),
+        String actual = Differ.generate(generateFixturePath("file1.yml"),
+                generateFixturePath("file2.yml"),
                 "json");
         assertEquals(jsonExpected, actual);
     }
