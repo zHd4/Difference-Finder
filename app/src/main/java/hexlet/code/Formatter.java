4e28ff2a36d88608dc7cb57b221935d275c548e0
zHd4 <computersystembase@gmail.com>
2023-11-02 02:57:55 +0100

    Refactor Formatter

--- app/src/main/java/hexlet/code/Formatter.java
+++ app/src/main/java/hexlet/code/Formatter.java
@@ -5,21 +5,13 @@ import hexlet.code.formatters.Json;
 import hexlet.code.formatters.Plain;
 import hexlet.code.formatters.Stylish;
 
-import java.util.Map;
-
 public class Formatter {
-    private static final Map<String, IFormatter> AVAILABLE_FORMATTERS = Map.of(
-            "stylish", new Stylish(),
-            "plain", new Plain(),
-            "json", new Json()
-    );
-
     public static IFormatter chooseFormatter(String format) {
-        if (!AVAILABLE_FORMATTERS.containsKey(format)) {
-            throw new IllegalArgumentException("Supported formats: "
-                    + String.join(", ", AVAILABLE_FORMATTERS.keySet()));
-        }
-
-        return AVAILABLE_FORMATTERS.get(format);
+        return switch (format) {
+            case "stylish" -> new Stylish();
+            case "plain" -> new Plain();
+            case "json" -> new Json();
+            default -> throw new IllegalArgumentException("Supported formats: stylish, plain, json");
+        };
     }
 }
